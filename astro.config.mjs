ae583691f894100d7f7d43b8428b76333df78d89
nordewal <nordewal@gmail.com>
2023-06-18 15:48:02 +0200

    set page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://uberlandpost.pages.dev',
   integrations: [mdx(), sitemap(), react()],
   experimental: {
     assets: true
