4b39dc4e12bd28868b2bb427ded273585d7237bd
nordewal <nordewal@gmail.com>
2023-09-13 10:53:25 +0500

    upgrade to astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://xn--berlandpost-shb.ch',
-  integrations: [mdx(), sitemap()],
-  experimental: {
-    assets: true
-   },
+  integrations: [],
   cacheDir: "cache",
 });
