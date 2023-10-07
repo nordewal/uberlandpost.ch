e4fb8e83c2e47367fdad468c64afa30e1f24b156
nordewal <nordewal@gmail.com>
2023-10-07 20:21:02 +0500

    Revert "Add cfbuild to allow prepopulating the cache on Cloudflare."
    Astro 3 does image generation in parallel. This is fast enough for now.
    And cloudflare is working on a proper build cache solution themselves.
    
    This reverts commit 66048274394da84902332cf2828e86e17f6fdf25.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://xn--berlandpost-shb.ch',
-  integrations: [],
-  cacheDir: "cache",
 });
--- bin/asset_list.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-(cd dist/_astro && ls *webp) > dist/_astro/assets.txt
